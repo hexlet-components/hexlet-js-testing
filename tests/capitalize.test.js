8182c2fd8292802799924ec48c11bd7bd1eba398
Roman Ashikov <rmn.shkv@gmail.com>
2021-10-22 15:30:35 +0700

    add jest

--- tests/capitalize.test.js
+++
@@ -1,5 +0,0 @@
-import { strict as assert } from 'assert';
-import { capitalize } from '../src/capitalize.js';
-
-assert.strictEqual(capitalize(''), '');
-assert.strictEqual(capitalize('hello'), 'Hello');
