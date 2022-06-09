e84dd478ecaccbaced79a0a5fb18a076897e12f5
Roman Ashikov <rmn.shkv@gmail.com>
2022-06-09 15:13:00 +0700

    translate rus to eng

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,11 @@
 import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
+  throw new Error('The function does not work correctly!');
 }
 
 if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
+  throw new Error('The function does not work correctly!');
 }
 
-console.log('Все тесты пройдены!');
+console.log('All tests passed!');
